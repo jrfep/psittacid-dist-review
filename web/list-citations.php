f1c3c00c53f8c5bf660d809089e7b6df35e27c45
jrfep <jr.ferrer.paris@gmail.com>
2021-05-17 12:14:25 +1000

    List cited references

--- web/list-citations.php
+++ web/list-citations.php
@@ -6,14 +6,14 @@ include("inc/hello.php");
 <?php
 $key=$_REQUEST["key"];
 
-$qry = "SELECT from_document,\"TI\",f1.reviewed_date rd1, f2.reviewed_date rd2, f3.reviewed_date rd3
+$qry = "SELECT from_document,\"TI\",f1.reviewed_date rd1, f2.reviewed_date rd2, f2.status, f3.reviewed_date rd3
   FROM psit.citation_rels
   LEFT JOIN psit.bibtex ON from_document=\"UT\"
   LEFT JOIN psit.filtro1 f1 ON from_document=f1.ref_id
   LEFT JOIN psit.filtro2 f2 ON from_document=f2.ref_id
   LEFT JOIN psit.distmodel_ref f3 ON from_document=f3.ref_id
   WHERE to_document='$key' AND relationship='UT cites SR'
-  AND f1.project='$project' AND f2.project='$project' 
+  AND f1.project='$project' AND f2.project='$project'
 ";
 
 
@@ -26,7 +26,7 @@ $qry = "SELECT from_document,\"TI\",f1.reviewed_date rd1, f2.reviewed_date rd2, 
  while ($row = pg_fetch_assoc($result)) {
           $tab .= "<TR><TH>".$row["TI"]."</TH>
           <TD>".$row["rd1"]."</TD>
-          <TD>".$row["rd2"]."</TD>
+          <TD>".$row["status"]." (".$row["rd2"].")</TD>
           <TD>".$row["rd3"]."</TD>
           <TD><a href='show-reference.php_UT=".$row["from_document"]."&project=$project'>Show</a></TD></TR>";
 
